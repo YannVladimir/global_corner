cf9358af5dd8369b1cceeb78fd406c5acd1ea26c
YannVladimir <bavlaya549@gmail.com>
2016-10-05 11:57:25 +0200

    login page edited

--- app/views/public/header.php
+++ app/views/public/header.php
@@ -65,7 +65,7 @@
 						<div class='shop-menu pull-right'>
 							<ul class='nav navbar-nav'>
 								<li><a></a></li>
-								<li><a href='public/loginpage'><i class='fa fa-lock'></i> Login</a></li>
+								<li><a href='loginpage'><i class='fa fa-lock'></i> Login</a></li>
 							</ul>
 						</div>
 					</div>";
