b13b2cb8d50201506ef82b1bca16eb9fcbada371
YannVladimir <bavlaya549@gmail.com>
2016-10-05 12:46:16 +0200

    hope it s done

--- public/index.php
+++ public/index.php
@@ -6,22 +6,22 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Home | Get It</title>
-    <link href="<?php echo BASEURL."assets/font-awesome/css/font-awesome.min.css"; ?>" rel="stylesheet" type="text/css">
-    <link href="<?php echo BASEURL."assets/css/yann.min.css"; ?>" rel="stylesheet">
-    <link href="<?php echo BASEURL."assets/css/prettyPhoto.css"; ?>" rel="stylesheet">
-    <link href="<?php echo BASEURL."assets/css/price-range.css"; ?>" rel="stylesheet">
-    <link href="<?php echo BASEURL."assets/css/animate.css"; ?>" rel="stylesheet">
-	<link href="<?php echo BASEURL."assets/css/main.css"; ?>" rel="stylesheet">
-	<link href="<?php echo BASEURL."assets/css/responsive.css"; ?>" rel="stylesheet">
+    <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/yann.min.css" rel="stylesheet">
+    <link href="assets/css/prettyPhoto.css" rel="stylesheet">
+    <link href="assets/css/price-range.css" rel="stylesheet">
+    <link href="assets/css/animate.css" rel="stylesheet">
+	<link href="assets/css/main.css" rel="stylesheet">
+	<link href="assets/css/responsive.css" rel="stylesheet">
     <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
     <![endif]-->   
-    <link rel="shortcut icon" href="<?php echo BASEURL."assets/images/ico/favicon.ico"; ?>">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo BASEURL."assets/images/ico/apple-touch-icon-144-precomposed.png"; ?>">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo BASEURL."assets/images/ico/apple-touch-icon-114-precomposed.png"; ?>">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo BASEURL."assets/images/ico/apple-touch-icon-72-precomposed.png"; ?>">
-    <link rel="apple-touch-icon-precomposed" href="<?php echo BASEURL."assets/images/ico/apple-touch-icon-57-precomposed.png"; ?>">
+    <link rel="shortcut icon" href="assets/images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/images/ico/apple-touch-icon-57-precomposed.png">
     <style type="text/css">
       .sizingimages{
         height: 200px;
@@ -354,11 +354,11 @@
 	<?php  
       require(BASEPATH.'app/views/public/footer.php');    
     ?>
-    <script src="<?php echo BASEURL."assets/js/jquery.js"; ?>"></script>
-    <script src="<?php echo BASEURL."assets/js/yann.min.js"; ?>"></script>
-	<script src="<?php echo BASEURL."assets/js/jquery.scrollUp.min.js"; ?>"></script>
-	<script src="<?php echo BASEURL."assets/js/price-range.js"; ?>"></script>
-    <script src="<?php echo BASEURL."assets/js/jquery.prettyPhoto.js"; ?>"></script>
-	<script src="<?php echo BASEURL."assets/js/main.js"; ?>"></script>
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/yann.min.js"></script>
+	<script src="assets/js/jquery.scrollUp.min.js"></script>
+	<script src="assets/js/price-range.js"></script>
+    <script src="assets/js/jquery.prettyPhoto.js"></script>
+	<script src="assets/js/main.js"></script>
 </body>
 </html>
