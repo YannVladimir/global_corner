4006c06aee2a529ea491401495e28140cd6dc5ba
YannVladimir <bavlaya549@gmail.com>
2016-10-05 12:33:51 +0200

    changed the model

--- public/loginpage.php
+++ public/loginpage.php
@@ -1,53 +1,122 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Login | Get It</title>
+    <link href="<?php echo BASEURL."assets/font-awesome/css/font-awesome.min.css"; ?>" rel="stylesheet" type="text/css">
+    <link href="<?php echo BASEURL."assets/css/yann.min.css"; ?>" rel="stylesheet">
+    <link href="<?php echo BASEURL."assets/css/prettyPhoto.css"; ?>" rel="stylesheet">
+    <link href="<?php echo BASEURL."assets/css/price-range.css"; ?>" rel="stylesheet">
+    <link href="<?php echo BASEURL."assets/css/animate.css"; ?>" rel="stylesheet">
+    <link href="<?php echo BASEURL."assets/css/main.css"; ?>" rel="stylesheet">
+    <link href="<?php echo BASEURL."assets/css/responsive.css"; ?>" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="<?php echo BASEURL."assets/images/ico/favicon.ico"; ?>">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo BASEURL."assets/images/ico/apple-touch-icon-144-precomposed.png"; ?>">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo BASEURL."assets/images/ico/apple-touch-icon-114-precomposed.png"; ?>">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo BASEURL."assets/images/ico/apple-touch-icon-72-precomposed.png"; ?>">
+    <link rel="apple-touch-icon-precomposed" href="<?php echo BASEURL."assets/images/ico/apple-touch-icon-57-precomposed.png"; ?>">
+    
+</head><!--/head-->
 
-class Loginpage extends Controller{
-	public function index()
-	{
-		$this->loadView('public/login');
-	}
-	public function authenticateUser()
-		{
-
-			//$password = sha1($_POST['password']);
-			// $password = md5($_POST['password']);
-			function clearInput($input)
-	        {
-		        return mysqli_real_escape_string(mysqli_connect("104.131.171.3","root","uIk3fDIL9q","eshopper"),htmlentities($input));
-	        }
-
-			require_once BASEPATH.'app/models/loginfunction.php';
-			$query= new Userlogin();
-			$email = clearInput($_POST['email']);
-			$password = clearInput($_POST['password']);
-			$res = $query->checkUser($email,$password);
-			$count = mysqli_num_rows($res);
-			if($count > 0)
-			{
-				$row = mysqli_fetch_assoc($res);
-				$_SESSION['id'] = $row['user_id'];
-			    $_SESSION['firstname'] = $row['firstname'];
-			    $_SESSION['lastname']=$row['lastname'];
-			    $_SESSION['phone']=$row['phone'];
-			    $_SESSION['email']=$row['email'];
-			    $_SESSION['password']=$row['password'];
-			    $_SESSION['priority']=$row['priority'];
-			    $_SESSION['is_admin']=$row['is_admin'];
-				//$_SESSION['user_is_admin']=$row['is_admin'];
-				if($row['is_admin']  == 1)
-				{
-					$_SESSION['user']="admin";
-					header("Location: ".BASEURL."dashboard");exit;
-					//echo "1";
-				}else
-				{
-					$_SESSION['user']="public";
-					header("Location: ".BASEURL."home");exit;
-					//echo "2";
-				}
-			}
-			else
-			{
-			echo "<script>alert(' email and password does not match, please try again');window.location='index';</script>";exit;
-			}
-		}
-}
+<body>
+	<?php  
+    require(BASEPATH.'app/views/public/header.php');   
+  ?>
+		
+    <div class="header-bottom"><!--header-bottom-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-9">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+						<div class="mainmenu pull-left">
+							<ul class="nav navbar-nav collapse navbar-collapse">
+								<li><a></a></li>
+								<li><a href="<?php echo BASEURL; ?>home">Home</a></li>
+								<li><a></a></li><li><a></a></li><li><a></a></li>
+								<li><a href="<?php echo BASEURL; ?>upload_electronics">Sell an item</a></li>
+								<li><a></a></li><li><a></a></li><li><a></a></li>
+								<li><a href="<?php echo BASEURL; ?>categories">Categories</a></li>
+								<li><a></a></li><li><a></a></li><li><a></a></li>
+								<li><a href="<?php echo BASEURL; ?>contact_us">Contact us</a></li>
+								
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="search_box">
+							<form action='/eshopper/public/results' method='GET'>
+              <input type="text" name='k' class="searchtext col-sm-10" placeholder="Search"/>
+              <button type="submit" class="btn btn-default col-sm-2 bton"><i class="fa fa-search"></i></button>
+              </form>
+            </div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-bottom-->
+	</header><!--/header-->
+	
+	<section id="form"><!--form-->
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4 col-sm-offset-1">
+					<div class="login-form"><!--login form-->
+						<h2>Login to your account</h2>
+						<form action="<?php echo BASEURL; ?>loginpage/authenticateUser" method="POST">
+							<input type="email" placeholder="Email Address" required="required" email="email" name="email" />
+							<input type="password" placeholder="password" required="required" name="password" />
+							<!--<span>
+								<input type="checkbox" class="checkbox"> 
+								Keep me signed in
+							</span>-->
+							<button type="submit" class="btn btn-default bton">Login</button>
+						</form>
+					</div><!--/login form-->
+				</div>
+				<div class="col-sm-1">
+					<h2 class="or">OR</h2>
+				</div>
+				<div class="col-sm-4">
+					<div class="signup-form"><!--sign up form-->
+						<h2>New User Signup!</h2>
+						<form action="newuser/create" method="post">
+							<input type="text" placeholder="First name" required="required" name="firstname"/>
+							<input type="text" placeholder="Last name" required="required" name="lastname"/>
+							<input type="text" placeholder="Contact number" required="required" digits="true" name="phone"/>
+							<input type="email" placeholder="Email Address" required="required" email="eamil" name="email"/>
+							<input type="password" placeholder="Password" required="required" name="password"/>
+							<input type="password" placeholder=" Re-type Password" required="required" equalTo="password" name="repassword"/>
+							<button type="submit" class="btn btn-default bton">Signup</button>
+						</form>
+					</div><!--/sign up form-->
+				</div>
+			</div>
+		</div>
+	</section><!--/form-->
+	
+	
+	<br><br><br><br><br><br><br>
+	<?php  
+      require(BASEPATH.'app/views/public/footer.php');    
+  ?>
+  <script src="<?php echo BASEURL."assets/js/jquery.js"; ?>"></script>
+  <script src="<?php echo BASEURL."assets/js/yann.min.js"; ?>"></script>
+  <script src="<?php echo BASEURL."assets/js/jquery.scrollUp.min.js"; ?>"></script>
+  <script src="<?php echo BASEURL."assets/js/jquery.prettyPhoto.js"; ?>"></script>
+  <script src="<?php echo BASEURL."assets/js/main.js"; ?>"></script>
+  <script src="<?php echo BASEURL."assets/js/jquery.validate.js"; ?>"></script>
+</body>
+</html>
