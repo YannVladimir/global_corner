3e1c2ba9933525853d0fe195a180570e0d5caff9
YannVladimir <bavlaya549@gmail.com>
2016-10-06 08:56:16 +0200

    show me the erro

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
+error_reporting(E_ALL);
 session_start();
 $con = mysqli_connect("104.131.171.3","root","uIk3fDIL9q","eshopper");
 ?>
