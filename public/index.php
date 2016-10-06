b5917a8ac03dc602ca1ad267c3fed76d662b5283
YannVladimir <bavlaya549@gmail.com>
2016-10-06 08:45:25 +0200

    ahfkjah

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
-
+<?php
+session_start();
+$con = mysqli_connect("104.131.171.3","root","uIk3fDIL9q","eshopper");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head> 
@@ -7,22 +10,22 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Home | Get It</title>
-    <link href="../assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="../assets/css/yann.min.css" rel="stylesheet">
-    <link href="../assets/css/prettyPhoto.css" rel="stylesheet">
-    <link href="../assets/css/price-range.css" rel="stylesheet">
-    <link href="../assets/css/animate.css" rel="stylesheet">
-	<link href="../assets/css/main.css" rel="stylesheet">
-	<link href="../assets/css/responsive.css" rel="stylesheet">
+    <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/yann.min.css" rel="stylesheet">
+    <link href="assets/css/prettyPhoto.css" rel="stylesheet">
+    <link href="assets/css/price-range.css" rel="stylesheet">
+    <link href="assets/css/animate.css" rel="stylesheet">
+	<link href="assets/css/main.css" rel="stylesheet">
+	<link href="assets/css/responsive.css" rel="stylesheet">
     <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
     <![endif]-->   
-    <link rel="shortcut icon" href="../assets/images/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="../assets/images/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="assets/images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/images/ico/apple-touch-icon-57-precomposed.png">
     <style type="text/css">
       .sizingimages{
         height: 200px;
@@ -133,7 +136,7 @@
 									<div class='product-image-wrapper'>
 										<div class='single-products'>
 											<div class='productinfo text-center'>
-												<img class='sizingimagesmax' src='../assets/images/subcategories/{$row['subcat_image']}' alt='' />
+												<img class='sizingimagesmax' src='assets/images/subcategories/{$row['subcat_image']}' alt='' />
 												<h4>{$row['subcat_name']}</h4>
 												<a href='certain?id={$row['subcat_id']}' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>Visit Category</a>
 											</div>	
@@ -160,7 +163,7 @@
 									<div class='product-image-wrapper'>
 										<div class='single-products'>
 											<div class='productinfo text-center'>
-												<img class='sizingimagesmax' src='../assets/images/subcategories/{$row['subcat_image']}' alt='' />
+												<img class='sizingimagesmax' src='assets/images/subcategories/{$row['subcat_image']}' alt='' />
 												<h4>{$row['subcat_name']}</h4>
 												<a href='certain?id={$row['subcat_id']}' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>Visit Category</a>
 											</div>	
@@ -188,7 +191,7 @@
 									<div class='product-image-wrapper'>
 										<div class='single-products'>
 											<div class='productinfo text-center'>
-												<img class='sizingimagesmax' src='../assets/images/subcategories/{$row['subcat_image']}' alt='' />
+												<img class='sizingimagesmax' src='assets/images/subcategories/{$row['subcat_image']}' alt='' />
 												<h4>{$row['subcat_name']}</h4>
 												<a href='certain?id={$row['subcat_id']}' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>Visit Category</a>
 											</div>	
@@ -216,7 +219,7 @@
 									<div class='product-image-wrapper'>
 										<div class='single-products'>
 											<div class='productinfo text-center'>
-												<img class='sizingimagesmax' src='../assets/images/subcategories/{$row['subcat_image']}' alt='' />
+												<img class='sizingimagesmax' src='assets/images/subcategories/{$row['subcat_image']}' alt='' />
 												<h4>{$row['subcat_name']}</h4>
 												<a href='certain?id={$row['subcat_id']}' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>Visit Category</a>
 											</div>	
@@ -243,7 +246,7 @@
 									<div class='product-image-wrapper'>
 										<div class='single-products'>
 											<div class='productinfo text-center'>
-												<img class='sizingimagesmax' src='../assets/images/subcategories/{$row['subcat_image']}' alt='' />
+												<img class='sizingimagesmax' src='assets/images/subcategories/{$row['subcat_image']}' alt='' />
 												<h4>{$row['subcat_name']}</h4>
 												<a href='certain?id={$row['subcat_id']}' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>Visit Category</a>
 											</div>	
@@ -271,7 +274,7 @@
 									<div class='product-image-wrapper'>
 										<div class='single-products'>
 											<div class='productinfo text-center'>
-												<img class='sizingimagesmax' src='../assets/images/subcategories/{$row['subcat_image']}' alt='' />
+												<img class='sizingimagesmax' src='assets/images/subcategories/{$row['subcat_image']}' alt='' />
 												<h4>{$row['subcat_name']}</h4>
 												<a href='certain?id={$row['subcat_id']}' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>Visit Category</a>
 											</div>	
@@ -299,7 +302,7 @@
 									<div class='product-image-wrapper'>
 										<div class='single-products'>
 											<div class='productinfo text-center'>
-												<img class='sizingimagesmax' src='../assets/images/subcategories/{$row['subcat_image']}' alt='' />
+												<img class='sizingimagesmax' src='assets/images/subcategories/{$row['subcat_image']}' alt='' />
 												<h4>{$row['subcat_name']}</h4>
 												<a href='certain?id={$row['subcat_id']}' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>Visit Category</a>
 											</div>	
@@ -328,7 +331,7 @@
 										<div class='single-products'>
 											<div class='productinfo text-center'>
 
-												<img class='sizingimagesmax' src='../assets/images/subcategories/{$row['subcat_image']}' alt='' />
+												<img class='sizingimagesmax' src='assets/images/subcategories/{$row['subcat_image']}' alt='' />
 												<h4>{$row['subcat_name']}</h4>
 												<a href='certain?id={$row['subcat_id']}' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>Visit Category</a>
 											</div>	
@@ -355,12 +358,12 @@
 	<?php  
       include('footer.php');    
     ?>
-    <script src="../assets/js/jquery.js"></script>
-    <script src="../assets/js/yann.min.js"></script>
-	<script src="../assets/js/jquery.scrollUp.min.js"></script>
-	<script src="../assets/js/price-range.js"></script>
-    <script src="../assets/js/jquery.prettyPhoto.js"></script>
-	<script src="../assets/js/main.js"></script>
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/yann.min.js"></script>
+	<script src="assets/js/jquery.scrollUp.min.js"></script>
+	<script src="assets/js/price-range.js"></script>
+    <script src="assets/js/jquery.prettyPhoto.js"></script>
+	<script src="assets/js/main.js"></script>
 </body>
 </html>
  	
