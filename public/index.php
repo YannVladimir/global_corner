9e99a0af62527ba75898f83609fd3e2dbae5ef05
YannVladimir <bavlaya549@gmail.com>
2016-10-17 10:29:06 +0200

    dskljfosaj

--- public/index.php
+++ public/index.php
@@ -3,7 +3,102 @@ ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 session_start();
-include('../includes/main_functions.php');
+  
+    $con = mysqli_connect("127.0.0.1","root","uIk3fDIL9q","eshopper");
+    // for redirecting 
+    function redirectTo($loc)
+    {
+        header('location:'.$loc);
+        exit;
+    }
+
+    function encodeUrl($url='')
+    {
+        if(!empty($url)) return encodeUrl($url);
+        return false;
+    }
+
+    function checkIsStringSetPost($name)
+    {
+        if(isset($_POST[$name]) && !empty($_POST[$name]) && is_string($_POST[$name]))
+            return true;
+        else return false;
+    }
+    function checkIsIntSetPost($name)
+    {
+        if(isset($_POST[$name]) && !empty($_POST[$name]) && is_numeric($_POST[$name]))
+            return true;
+        else return false;
+    }
+    function clearInput($input)
+    {
+        return mysqli_real_escape_string(mysqli_connect("127.0.0.1","root","uIk3fDIL9q","eshopper"),htmlentities($input));
+    }
+    function log_user_in($email, $password)
+    {
+        $email = clearInput($email);
+        $password = clearInput($password);
+        $query="SELECT * from users where email =\"$email\" and password = \"$password\"";
+        $res = mysqli_query($con,$query);
+        if(mysqli_num_rows($res) == 1)
+        {
+            $row = mysqli_fetch_assoc($res);
+            $_SESSION['id'] = $row['user_id'];
+            $_SESSION['username'] = $row['firstname'].' '.$row['lastname'];
+            $_SESSION['phone']=$row['phone'];
+            $_SESSION['email']=$email;
+            $_SESSION['priority']=$row['priority'];
+            if($row['is_admin']  == 1)
+            {
+                $_SESSION['admin'] = "01";
+                header("location: ../admin/dashboard.php");
+            }
+            else
+            {
+                $_SESSION['admin'] == "00";
+                header("location: home.php");
+            }
+            exit(); 
+        }
+        else
+        {
+            $_SESSION['message'] = "Wrong email/password combination";
+            header("location: login.php");
+        }exit();        
+    }
+
+    function log_user_out()
+    {
+        session_destroy();
+        unset($_SESSION['id']);
+        unset($_SESSION['username']);
+        unset($_SESSION['phone']);
+        unset($_SESSION['email']);
+        unset($_SESSION['priority']);
+    }
+
+    function log_user_out_admin()
+    {
+        session_destroy();
+        unset($_SESSION['id']);
+        unset($_SESSION['username']);
+        unset($_SESSION['phone']);
+        unset($_SESSION['email']);
+        unset($_SESSION['priority']);
+        header("location: ../home.php");
+    }exit();
+
+    function checkAdmin()
+    {
+        if (isset($_SESSION['admin']) =="01")
+        {
+           return true;
+        }
+        else
+            header("location: ../home.php");
+    }
+    exit();
+
 if(isset($_GET['var']) == "logout")
 {
     log_user_out();
