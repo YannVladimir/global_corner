1432ceb1e34386597fb1f45d1f27d9c97d0451f3
YannVladimir <bavlaya549@gmail.com>
2016-10-25 13:07:35 +0200

    DVAADF

--- public/admin/dashboard.php
+++ public/admin/dashboard.php
@@ -31,30 +31,74 @@
                                            $query="SELECT * from users";
                                            $res = mysqli_query($con,$query);
                                            $count = 0;
+                                           $a = 0;
                                            while($row = mysqli_fetch_assoc($res))
                                            {
                                             if($row['accepted']== 0)
                                               {
                                                 $count++;
-                                               }    
+                                               }
+                                               $a++;   
                                             }
-                                            echo " $count";
+                                            echo " $count
 
-                                            ?> 
+                                             
                                         </div>
-                                        <div>New Users</div>
+                                        <div>$a Total</div>";?>
                                     </div>
                                 </div>
                             </div>
                             <a href="new-users.php">
                                 <div class="panel-footer">
-                                    <span class="pull-left">View Details</span>
-                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <span class="pull-left">Users </span>
+                                    <span class="pull-right">View Details <i class="fa fa-arrow-circle-right"></i></span>
                                     <div class="clearfix"></div>
                                 </div>
                             </a>
                         </div>
                     </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-yellow">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-tasks fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">
+                                         <?php 
+                                           $query = "SELECT * FROM items where refcat_id = 10";
+                                           $res = mysqli_query($con,$query);
+                                           $count = 0;
+                                           $a = 0;
+                                           while($row = mysqli_fetch_assoc($res))
+                                           {
+                                            $count++;
+                                            if($row['is_accepted']==0){
+                                            $a++;}     
+                                           }
+                                           echo " $a";
+                                           echo "</div>";
+                                           echo "<div>";
+                                           echo "$count ";
+                                           echo " Total</div>"
+                                          ?>
+                                        
+                                       
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="jobs.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">Messages</span>
+                                    <span class="pull-right">View Details <i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
                     <div class="col-lg-3 col-md-6">
                         <div class="panel panel-green">
                             <div class="panel-heading">
@@ -478,80 +522,6 @@
                                 </div>
                             </div>
                             <a href="jobs.php">
-                                <div class="panel-footer">
-                                    <span class="pull-left">View Details</span>
-                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                    <div class="clearfix"></div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="panel panel-red">
-                            <div class="panel-heading">
-                                <div class="row">
-                                    <div class="col-xs-3">
-                                        <i class="fa fa-envelope fa-5x"></i>
-                                    </div>
-                                    <div class="col-xs-9 text-right">
-                                        <div class="huge">
-                                           <?php 
-                                           $query = "SELECT * FROM contactus ";
-                                           $res = mysqli_query($con,$query);
-                                           $count = 0;
-                                           while($row = mysqli_fetch_assoc($res))
-                                           {
-                                            if($row['seen']== 0)
-                                              {
-                                                $count++;
-                                               }    
-                                            }
-                                            echo " $count";
-
-                                            ?>
-                                        </div>
-                                        <div>Messages</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <a href="new-messages.php">
-                                <div class="panel-footer">
-                                    <span class="pull-left">View Details</span>
-                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                    <div class="clearfix"></div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="panel panel-red">
-                            <div class="panel-heading">
-                                <div class="row">
-                                    <div class="col-xs-3">
-                                        <i class="fa fa-envelope fa-5x"></i>
-                                    </div>
-                                    <div class="col-xs-9 text-right">
-                                        <div class="huge">
-                                           <?php 
-                                           $query = "SELECT * FROM orders ";
-                                           $res = mysqli_query($con,$query);
-                                           $count = 0;
-                                           while($row = mysqli_fetch_assoc($res))
-                                           {
-                                            if($row['is_accepted']== 0)
-                                              {
-                                                $count++;
-                                               }    
-                                            }
-                                            echo " $count";
-
-                                            ?>
-                                        </div>
-                                        <div>New Orders</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <a href="new-messages.php">
                                 <div class="panel-footer">
                                     <span class="pull-left">View Details</span>
                                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
