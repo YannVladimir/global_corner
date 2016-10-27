6d97e2e52a61bc5f3da08a0358341118b6b571ff
YannVladimir <bavlaya549@gmail.com>
2016-10-27 08:20:44 +0200

    DSG

--- public/sms.php
+++ public/sms.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
+error_reporting(E_ALL);
   require('class-Clockwork.php');
   $apikey = "b323f6b70ca11f37241ae41ae9f9dc9f162a5ad3";
   $clockwork = new Clockwork($apikey);
