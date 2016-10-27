be04e15f86ae09f23c718b66d164b719cf6dad7c
YannVladimir <bavlaya549@gmail.com>
2016-10-27 08:11:43 +0200

    DFAS

--- public/sms.php
+++ public/sms.php
@@ -1,7 +1,7 @@
 <?php
-  require('class-Clorkwork.php');
+  require('class-Clockwork.php');
   $apikey = "b323f6b70ca11f37241ae41ae9f9dc9f162a5ad3";
-  $clorkwork = new Clorkwork($apikey);
+  $clockwork = new Clockwork($apikey);
   $message = array('to' => '+250782767289', 'message' => 'done yann from 250trade.com');
-  $done = $clorkwork->send($message);
+  $done = $clockwork->send($message);
 ?>
