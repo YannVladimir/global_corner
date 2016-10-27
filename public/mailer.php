5b15e422586b505b4496ed12b45f25d01a09fba1
YannVladimir <bavlaya549@gmail.com>
2016-10-27 09:27:21 +0200

    fjnak

--- public/mailer.php
+++ public/mailer.php
@@ -2,7 +2,7 @@
 ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(E_ALL);
-require 'class.phpmailer.php';
+require_once('PHPMailer/PHPMailerAutoload.php');
  
 // Instantiate Class
 $mail = new PHPMailer();
