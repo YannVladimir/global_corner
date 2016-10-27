6b62e985931ffbeb70840aad7b18d668d31fafd9
YannVladimir <bavlaya549@gmail.com>
2016-10-27 08:17:22 +0200

    sfajfk

--- public/sms.php
+++ public/sms.php
@@ -2,6 +2,6 @@
   require('class-Clockwork.php');
   $apikey = "b323f6b70ca11f37241ae41ae9f9dc9f162a5ad3";
   $clockwork = new Clockwork($apikey);
-  $message = array('to' => '+250782767289', 'message' => 'done yann from 250trade.com');
+  $message = array('to' => '250782767289', 'message' => 'done yann from 250trade.com');
   $done = $clockwork->send($message);
 ?>
