01875dbfedd11fbea3286e8ea5d563881ba9b744
YannVladimir <bavlaya549@gmail.com>
2016-10-29 19:48:09 +0200

    FSAF

--- public/send.php
+++ public/send.php
@@ -2,7 +2,7 @@
 ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(E_ALL);
- $to = "250782767289@vtext.com";
+ $to = "bajenezayann549@gmail.com";
  $from = "bavlaya549@gmail.com";
  $message = "done";
  $headers = "From: $from\n"; 
