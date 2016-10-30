ab7d9c665cd23bbc7898de4c120fb5a42694fe72
YannVladimir <bavlaya549@gmail.com>
2016-10-30 17:48:34 +0200

    hbjhb

--- public/index.php
+++ public/index.php
@@ -47,9 +47,6 @@ if(isset($_GET['var']) == "logout")
       }
       .fon{
         font-size: 20px;
-        border-right-style: solid;
-        border-right-width:1px;
-        border-right-color:black;
       }
     </style>
 </head><!--/head-->
@@ -91,7 +88,10 @@ if(isset($_GET['var']) == "logout")
                         <div class="search_box">
                           <form action='search_results.php' method='GET'>
                             <div class="col-sm-1"></div>
-                            <select class="col-sm-4" style="max-height:36px;">
+                            <select class="col-sm-4" style="height:36px;
+        border-right-style: solid;
+        border-right-width:1px;
+        border-right-color:black;">
                               <option>All categories</option>
                               <?php
                                 $q = "SELECT * from categories";
