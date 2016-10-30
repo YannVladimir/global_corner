f676ca20b2766db0a993d0508bd02bbd6d3da06b
YannVladimir <bavlaya549@gmail.com>
2016-10-30 17:12:55 +0200

    FSDS

--- public/index.php
+++ public/index.php
@@ -87,11 +87,12 @@ if(isset($_GET['var']) == "logout")
                     <div class="col-sm-5">
                         <div class="search_box">
                           <form action='search_results.php' method='GET'>
+                            <div class="col-sm-1"></div>
                             <select class="col-sm-4">
                               <option>All categories</option>
                             </select>
                             <input type="text" name='k'  required="required" class="col-sm-6" placeholder="Search"/>
-                            <button type="submit" class="btn btn-default col-sm-2 bton"><i class="fa fa-search"></i></button>
+                            <button type="submit" class="btn btn-default col-sm-1 bton"><i class="fa fa-search"></i></button>
                           </form>
                         </div>
                     </div>
