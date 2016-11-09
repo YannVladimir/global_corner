2ffd894b8ec996b1c55396c66fd4f46a558a2545
YannVladimir <bavlaya549@gmail.com>
2016-11-09 08:13:56 +0200

    SS

--- public/upload_services.php
+++ public/upload_services.php
@@ -257,7 +257,6 @@ checkToken();
   <script src="assets/js/yann.min.js"></script>
   <script src="assets/js/jquery.scrollUp.min.js"></script>
   <script src="assets/js/jquery.prettyPhoto.js"></script>
-  <script src="assets/js/main.js"></script>
   <script src="assets/js/jquery.validate.js"></script>
   <script src="assets/js/uploading.js"></script>
   <script src="assets/js/image.upload.js"></script>
