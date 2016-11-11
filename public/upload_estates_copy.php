0e68086b8f8352124c1b0f153c4c3fc18e10779c
YannVladimir <bavlaya549@gmail.com>
2016-11-11 13:51:09 +0200

    DFD

--- public/upload_estates_copy.php
+++ public/upload_estates_copy.php
@@ -217,10 +217,11 @@ checkToken();
                         </div>
                         <div class="col-sm-4"> 
                                   <input type="file" name="main" class="this" id="inp" />
-                                  <img id="image" class="btn1 starting" />
-                                <!--  <progress id="prog" max="100" value="0" style="display:none;"></progress>
+                                  <progress id="prog" max="100" value="0" style="display:none;"></progress>
                                   <div id="percent"><div>
-                                  <div id="here"></div>-->
+                                  <div id="here"></div>
+                                  <img id="image" class="btn1 starting" />
+                                  
                             </div>
                             <div class="col-sm-4">
                                   <input type="file" name="img1" class="this" id="inp2" />
