645f5488b1e7ce5086c2ed36a1f4006a59cb857a
YannVladimir <bavlaya549@gmail.com>
2016-11-18 14:31:35 +0200

    VDDSGSD

--- public/upload.php
+++ public/upload.php
@@ -2,6 +2,7 @@
 ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(E_ALL);
+session_start();
 require_once ('../includes/main_functions.php');
 //checkUser();
 if(isset($_GET['id'])){
