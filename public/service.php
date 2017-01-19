01ab409e2e2033c34130bd454e3e645a112c6f8e
YannVladimir <bavlaya549@gmail.com>
2017-01-19 09:21:29 +0200

    A

--- public/service.php
+++ public/service.php
@@ -472,92 +472,151 @@ require_once ('../includes/main_functions.php');
                     <div class="recommended_items"><!--recommended_items-->
                         <h2 class="title text-center">recommended sellers</h2><!--shyiramo s_cat name hagati ya recommended na sellers-->
                         
-                        <div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
-                            <div class="carousel-inner">
-                                <div class="item active">   
-                                    <div class="col-sm-4">
-                                        <div class="product-image-wrapper">
-                                            <div class="single-products">
-                                                <div class="productinfo text-center">
-                                                    <img src="images/home/recommend1.jpg" alt="" />
-                                                    <h2>$56</h2>
-                                                    <p>Easy Polo Black Edition</p>
-                                                    <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
-                                                </div>
+                        <?php 
+                       $query = "SELECT * FROM amaservice where is_accepted = 1 order by priority limit 20";
+                       $res = mysqli_query($con,$query);
+                       while($row = mysqli_fetch_assoc($res))
+                       {
+                        if($row['main'] == 'noimage.jpg' || $row['main'] == '' )
+                        {
+                              if($row['avg']==0){
+                                $img = '<b>Rank:</b> Not available <br>';
+                              }
+                              elseif($row['avg']<=1.25)
+                              {
+                                $img = '<img src="assets/images/shop/rating2.png" alt="" />';
+                              }
+                              elseif ($row['avg']>1.25 && $row['avg']<1.75) {
+                                $img = '<img src="assets/images/shop/rating3.png" alt="" />';
+                              }
+                              elseif ($row['avg']>1.75 && $row['avg']<2.25) {
+                                $img = '<img src="assets/images/shop/rating4.png" alt="" />';
+                              }
+                              elseif ($row['avg']>2.25 && $row['avg']<2.75) {
+                                $img = '<img src="assets/images/shop/rating5.png" alt="" />';
+                              }
+                              elseif ($row['avg']>2.75 && $row['avg']<3.25) {
+                                $img = '<img src="assets/images/shop/rating6.png" alt="" />';
+                              }
+                              elseif ($row['avg']>3.25 && $row['avg']<3.75) {
+                                $img = '<img src="assets/images/shop/rating7.png" alt="" />';
+                              }
+                              elseif ($row['avg']>3.75 && $row['avg']<4.25) {
+                                $img = '<img src="assets/images/shop/rating8.png" alt="" />';
+                              }
+                              elseif ($row['avg']>4.25 && $row['avg']<4.75) {
+                                $img = '<img src="assets/images/shop/rating9.png" alt="" />';
+                              }
+                              else{
+                                $img = '<img src="assets/images/shop/rating10.png" alt="" />';
+                              }
+
+                        echo "<br><a href='service.php?id={$row['id']}'>
+                        <div class='row' style='border: 1px solid #F7F7F0; background:#f6f6f6'>";
+                            echo "<div class='col-sm-5'>
+                                     <div class='product-image-wrapper'>
+                                        <div class='single-products'>
+                                            <div class='productinfo text-center'>
+                                                 <div class='sizingimages'>
+                                                    <img class='sizingimagesmax' src='assets/images/posts/noimage.png' alt='' class=''/>
+                                                  </div>
+                                            </div>
+                                            <b> {$row['category']} - {$row['sub_category']}</b>
+                                            <div class='product-overlay' style='opacity:0.9'>
+                                                 <div class='overlay-content'>
+                                                     <a href='service.php?id={$row['id']}' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>View Details</a>
+                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="col-sm-4">
-                                        <div class="product-image-wrapper">
-                                            <div class="single-products">
-                                                <div class="productinfo text-center">
-                                                    <img src="images/home/recommend2.jpg" alt="" />
-                                                    <h2>$56</h2>
-                                                    <p>Easy Polo Black Edition</p>
-                                                    <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
-                                                </div>
+                                     </div>
+                                  </div>
+                                  <div class='col-sm-7'>
+                                      <div class='product-information' style='border-left-style:none;border-bottom-style:none'><!--/product-information-->
+                                        {$img}<br> <b>{$row['avg']} stars | </b>
+                                        <b>Total votes: {$row['total_votes']}</b><br>
+                                        <span>
+                                             <h2>{$row['reserved']}</h2>
+                                        </span>
+                                        <p>Contact number:<b> {$row['phone']}</b></p>
+                                        <p>Place:<b> {$row['place_name']} - {$row['location']}</b></p>
+                                        <p><b></b></p><br>
+                                        <p style='text-align:center;'><a style='background:#90DC60; color:white;' href='service.php?id={$row['id']}' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>View Details</a><p>
+                                        </div><!--/product-information-->
+            </div>";
+
+                        echo "</div></a>";
+                        }
+                        else
+                        {
+                          if($row['avg']==0){
+                                $img = '<b>Rank:</b> Not available <br>';
+                              }
+                              elseif($row['avg']<=1.25)
+                              {
+                                $img = '<img src="assets/images/shop/rating2.png" alt="" />';
+                              }
+                              elseif ($row['avg']>1.25 && $row['avg']<1.75) {
+                                $img = '<img src="assets/images/shop/rating3.png" alt="" />';
+                              }
+                              elseif ($row['avg']>1.75 && $row['avg']<2.25) {
+                                $img = '<img src="assets/images/shop/rating4.png" alt="" />';
+                              }
+                              elseif ($row['avg']>2.25 && $row['avg']<2.75) {
+                                $img = '<img src="assets/images/shop/rating5.png" alt="" />';
+                              }
+                              elseif ($row['avg']>2.75 && $row['avg']<3.25) {
+                                $img = '<img src="assets/images/shop/rating6.png" alt="" />';
+                              }
+                              elseif ($row['avg']>3.25 && $row['avg']<3.75) {
+                                $img = '<img src="assets/images/shop/rating7.png" alt="" />';
+                              }
+                              elseif ($row['avg']>3.75 && $row['avg']<4.25) {
+                                $img = '<img src="assets/images/shop/rating8.png" alt="" />';
+                              }
+                              elseif ($row['avg']>4.25 && $row['avg']<4.75) {
+                                $img = '<img src="assets/images/shop/rating9.png" alt="" />';
+                              }
+                              else{
+                                $img = '<img src="assets/images/shop/rating10.png" alt="" />';
+                              }
+
+                        echo "<br><a href='service.php?id={$row['id']}'>
+                        <div class='row' style='border: 1px solid #F7F7F0; background:#f6f6f6'>";
+                            echo "<div class='col-sm-5'>
+                                     <div class='product-image-wrapper'>
+                                        <div class='single-products'>
+                                            <div class='productinfo text-center'>
+                                                 <div class='sizingimages'>
+                                                    <img class='sizingimagesmax' src='assets/images/posts/{$row['main']}' alt='' class=''/>
+                                                  </div>
+                                            </div>
+                                            <b> {$row['category']} - {$row['sub_category']}</b>
+                                            <div class='product-overlay' style='opacity:0.9'>
+                                                 <div class='overlay-content'>
+                                                     <a href='service.php?id={$row['id']}' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>View Details</a>
+                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="col-sm-4">
-                                        <div class="product-image-wrapper">
-                                            <div class="single-products">
-                                                <div class="productinfo text-center">
-                                                    <img src="images/home/recommend3.jpg" alt="" />
-                                                    <h2>$56</h2>
-                                                    <p>Easy Polo Black Edition</p>
-                                                    <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="item">  
-                                    <div class="col-sm-4">
-                                        <div class="product-image-wrapper">
-                                            <div class="single-products">
-                                                <div class="productinfo text-center">
-                                                    <img src="images/home/recommend1.jpg" alt="" />
-                                                    <h2>$56</h2>
-                                                    <p>Easy Polo Black Edition</p>
-                                                    <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-4">
-                                        <div class="product-image-wrapper">
-                                            <div class="single-products">
-                                                <div class="productinfo text-center">
-                                                    <img src="images/home/recommend2.jpg" alt="" />
-                                                    <h2>$56</h2>
-                                                    <p>Easy Polo Black Edition</p>
-                                                    <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-4">
-                                        <div class="product-image-wrapper">
-                                            <div class="single-products">
-                                                <div class="productinfo text-center">
-                                                    <img src="images/home/recommend3.jpg" alt="" />
-                                                    <h2>$56</h2>
-                                                    <p>Easy Polo Black Edition</p>
-                                                    <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                             <a class="left recommended-item-control" href="#recommended-item-carousel" data-slide="prev">
-                                <i class="fa fa-angle-left"></i>
-                              </a>
-                              <a class="right recommended-item-control" href="#recommended-item-carousel" data-slide="next">
-                                <i class="fa fa-angle-right"></i>
-                              </a>          
-                        </div>
+                                     </div>
+                                  </div>
+                                  <div class='col-sm-7'>
+                                      <div class='product-information' style='border-left-style:none;border-bottom-style:none'><!--/product-information-->
+                                        {$img}<br> <b>{$row['avg']} stars | </b>
+                                        <b>Total votes: {$row['total_votes']}</b><br>
+                                        <span>
+                                             <h2>{$row['reserved']}</h2>
+                                        </span>
+                                        <p>Contact number:<b> {$row['phone']}</b></p>
+                                        <p>Place:<b> {$row['place_name']} - {$row['location']}</b></p>
+                                        <p><b></b></p><br>
+                                        <p style='text-align:center;'><a style='background:#90DC60; color:white;' href='service.php?id={$row['id']}' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>View Details</a><p>
+                                        </div><!--/product-information-->
+            </div>";
+
+                        echo "</div></a>";
+                        }
+                       }
+                    ?>
                     </div><!--/recommended_items-->
                     
                 </div>
