a5ee7d8bf714318ca1601effed42076f75cc8279
YannVladimir <bavlaya549@gmail.com>
2017-01-25 11:17:03 +0200

    AFF

--- public/service-sub-category.php
+++ public/service-sub-category.php
@@ -8,7 +8,7 @@ require_once ('../includes/main_functions.php');
 ?>
 <!DOCTYPE html> 
 <html lang="en">
-<head>
+<head> 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
@@ -86,8 +86,8 @@ require_once ('../includes/main_functions.php');
             <h2 class="title text-center">Service categories</h2>
             <div class="panel-group category-products" id="accordian"><!--category-productsr-->
               <?php 
-                                        $c = 1;
-                                        $query = "SELECT * FROM service_categories ";
+                                        $c = 11;
+                                        $query = "SELECT * FROM service_categories";
                                         $res = mysqli_query($con,$query);
                                         while($row = mysqli_fetch_assoc($res))
                                         {
@@ -129,22 +129,62 @@ require_once ('../includes/main_functions.php');
             
           </div>
         </div>
-        <div class="col-sm-6 padding-right">
+        
+        <div class="col-sm-5 padding-right">
+          <br><br><br>
+          <h2 class="title text-center">Recomended service providers</h2>
                     <br>
-                    <?php
-                       $query = "SELECT * FROM services where is_accepted=0 order by priority desc limit 20";
+                    <?php 
+                       $id = $_GET['id'];
+                       $query = "SELECT * FROM amaservice where is_accepted = 1 and category = '{$id}' order by total_marks desc limit 20";
                        $res = mysqli_query($con,$query);
                        while($row = mysqli_fetch_assoc($res))
                        {
-                        echo "<br><div class='row' style='border: 1px solid #F7F7F0; background:#f6f6f6'>";
-                            echo "<div class='col-sm-6'>
+                        if($row['main'] == 'noimage.jpg' || $row['main'] == '' )
+                        {
+                              if($row['avg']==0){
+                                $img = '<b>Rank:</b> Not available <br>';
+                              }
+                              elseif($row['avg']<=1.25)
+                              {
+                                $img = '<img src="assets/images/shop/rating2.png" alt="" />';
+                              }
+                              elseif ($row['avg']>=1.25 && $row['avg']<1.75) {
+                                $img = '<img src="assets/images/shop/rating3.png" alt="" />';
+                              }
+                              elseif ($row['avg']>=1.75 && $row['avg']<2.25) {
+                                $img = '<img src="assets/images/shop/rating4.png" alt="" />';
+                              }
+                              elseif ($row['avg']>=2.25 && $row['avg']<2.75) {
+                                $img = '<img src="assets/images/shop/rating5.png" alt="" />';
+                              }
+                              elseif ($row['avg']>=2.75 && $row['avg']<3.25) {
+                                $img = '<img src="assets/images/shop/rating6.png" alt="" />';
+                              }
+                              elseif ($row['avg']>=3.25 && $row['avg']<3.75) {
+                                $img = '<img src="assets/images/shop/rating7.png" alt="" />';
+                              }
+                              elseif ($row['avg']>=3.75 && $row['avg']<4.25) {
+                                $img = '<img src="assets/images/shop/rating8.png" alt="" />';
+                              }
+                              elseif ($row['avg']>=4.25 && $row['avg']<4.75) {
+                                $img = '<img src="assets/images/shop/rating9.png" alt="" />';
+                              }
+                              else{
+                                $img = '<img src="assets/images/shop/rating10.png" alt="" />';
+                              }
+
+                        echo "<br><a href='service.php?id={$row['id']}'>
+                        <div class='row' style='border: 1px solid #F7F7F0; background:#f6f6f6'>";
+                            echo "<div class='col-sm-5'>
                                      <div class='product-image-wrapper'>
                                         <div class='single-products'>
                                             <div class='productinfo text-center'>
                                                  <div class='sizingimages'>
-                                                    <img class='sizingimagesmax' src='assets/images/posts/{$row['main']}' alt='' class=''/>
+                                                    <img class='sizingimagesmax' src='assets/images/posts/noimage.png' alt='' class=''/>
                                                   </div>
                                             </div>
+                                            <b> {$row['category']} - {$row['sub_category']}</b>
                                             <div class='product-overlay' style='opacity:0.9'>
                                                  <div class='overlay-content'>
                                                      <a href='service.php?id={$row['id']}' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>View Details</a>
@@ -153,19 +193,91 @@ require_once ('../includes/main_functions.php');
                                         </div>
                                      </div>
                                   </div>
-                                  <div class='col-sm-6'>
+                                  <div class='col-sm-7'>
                                       <div class='product-information' style='border-left-style:none;border-bottom-style:none'><!--/product-information-->
-                                        <img src='assets/images/shop/rating7.png' class='newarrival' alt='' />
+                                        {$img}<br> <b>{$row['avg']} stars | </b>
+                                        <b>Total votes: {$row['total_votes']}</b><br>
                                         <span>
-                                          <h2>{$row['reserved']}</h2>
+                                             <h2>{$row['reserved']}</h2>
                                         </span>
                                         <p>Contact number:<b> {$row['phone']}</b></p>
-                                        <p>Place:<b> {$row['Akarere']} - {$row['location']}</b></p>
-                                        <p><b></b></p>
+                                        <p>Place:<b> {$row['place_name']} - {$row['location']}</b></p>
+                                        <p><b></b></p><br>
+                                        <p style='text-align:center;'><a style='background:#90DC60; color:white;' href='service.php?id={$row['id']}' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>View Details</a><p>
                                         </div><!--/product-information-->
             </div>";
 
-                        echo "</div>";
+                        echo "</div></a>";
+                        }
+                        else
+                        {
+                          if($row['avg']==0){
+                                $img = '<b>Rank:</b> Not available <br>';
+                              }
+                              elseif($row['avg']<=1.25)
+                              {
+                                $img = '<img src="assets/images/shop/rating2.png" alt="" />';
+                              }
+                              elseif ($row['avg']>=1.25 && $row['avg']<1.75) {
+                                $img = '<img src="assets/images/shop/rating3.png" alt="" />';
+                              }
+                              elseif ($row['avg']>=1.75 && $row['avg']<2.25) {
+                                $img = '<img src="assets/images/shop/rating4.png" alt="" />';
+                              }
+                              elseif ($row['avg']>=2.25 && $row['avg']<2.75) {
+                                $img = '<img src="assets/images/shop/rating5.png" alt="" />';
+                              }
+                              elseif ($row['avg']>=2.75 && $row['avg']<3.25) {
+                                $img = '<img src="assets/images/shop/rating6.png" alt="" />';
+                              }
+                              elseif ($row['avg']>=3.25 && $row['avg']<3.75) {
+                                $img = '<img src="assets/images/shop/rating7.png" alt="" />';
+                              }
+                              elseif ($row['avg']>=3.75 && $row['avg']<4.25) {
+                                $img = '<img src="assets/images/shop/rating8.png" alt="" />';
+                              }
+                              elseif ($row['avg']>=4.25 && $row['avg']<4.75) {
+                                $img = '<img src="assets/images/shop/rating9.png" alt="" />';
+                              }
+                              else{
+                                $img = '<img src="assets/images/shop/rating10.png" alt="" />';
+                              }
+
+                        echo "<br><a href='service.php?id={$row['id']}'>
+                        <div class='row' style='border: 1px solid #F7F7F0; background:#f6f6f6'>";
+                            echo "<div class='col-sm-5'>
+                                     <div class='product-image-wrapper'>
+                                        <div class='single-products'>
+                                            <div class='productinfo text-center'>
+                                                 <div class='sizingimages'>
+                                                    <img class='sizingimagesmax' src='assets/images/posts/{$row['main']}' alt='' class=''/>
+                                                  </div>
+                                            </div>
+                                            <b> {$row['category']} - {$row['sub_category']}</b>
+                                            <div class='product-overlay' style='opacity:0.9'>
+                                                 <div class='overlay-content'>
+                                                     <a href='service.php?id={$row['id']}' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>View Details</a>
+                                                 </div>
+                                            </div>
+                                        </div>
+                                     </div>
+                                  </div>
+                                  <div class='col-sm-7'>
+                                      <div class='product-information' style='border-left-style:none;border-bottom-style:none'><!--/product-information-->
+                                        {$img}<br> <b>{$row['avg']} stars | </b>
+                                        <b>Total votes: {$row['total_votes']}</b><br>
+                                        <span>
+                                             <h2>{$row['reserved']}</h2>
+                                        </span>
+                                        <p>Contact number:<b> {$row['phone']}</b></p>
+                                        <p>Place:<b> {$row['place_name']} - {$row['location']}</b></p>
+                                        <p><b></b></p><br>
+                                        <p style='text-align:center;'><a style='background:#90DC60; color:white;' href='service.php?id={$row['id']}' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>View Details</a><p>
+                                        </div><!--/product-information-->
+            </div>";
+
+                        echo "</div></a>";
+                        }
                        }
                     ?>
       </div>
