42355c21bf7bef6ffee082a5d4d7242cdb03fdc8
YannVladimir <bavlaya549@gmail.com>
2017-01-31 06:32:35 +0200

    ADA

--- public/home.php
+++ public/home.php
@@ -744,8 +744,8 @@ if(isset($_GET['var']) == "logout")
 </div><!--/row-->
 </div>
 </div>
-<div class="row">
-        <div class="col-sm-16 pull-left">
+<div class="row"></div></div><div class="row">
+        <div class="col-sm-12 pull-left">
           <div class="col-sm-8 pull-left">
           <h2 class="title text-center">Recomended service providers</h2>
 
