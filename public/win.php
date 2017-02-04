c68243e683eded95b034a4f608b3b565837b44ae
YannVladimir <bavlaya549@gmail.com>
2017-02-04 17:12:45 +0200

    AF

--- public/win.php
+++ public/win.php
@@ -146,76 +146,10 @@ require_once ('../includes/main_functions.php');
                </ol> </p>
               <br><br>
               <p class="text-left">
-               <b>Time left for first prizes:</b> 
-                <script language="JavaScript">
-              TargetDate = "02/15/2017 00:00 AM";
-BackColor = "white";
-ForeColor = "#3AACEB";
-CountActive = true;
-CountStepper = -1;
-LeadingZero = true;
-DisplayFormat = "<br>Time left: %%D%% Days, %%H%% Hours, %%M%% Minutes, %%S%% Seconds.";
-FinishMessage = "Make rwanda know what you trade";
-function calcage(secs, num1, num2) {
-  s = ((Math.floor(secs/num1))%num2).toString();
-  if (LeadingZero && s.length < 2)
-    s = "0" + s;
-  return "<b>" + s + "</b>";
-}
-
-function CountBack(secs) {
-  if (secs < 0) {
-    document.getElementById("cntdwn").innerHTML = FinishMessage;
-    return;
-  }
-  DisplayStr = DisplayFormat.replace(/%%D%%/g, calcage(secs,86400,100000));
-  DisplayStr = DisplayStr.replace(/%%H%%/g, calcage(secs,3600,24));
-  DisplayStr = DisplayStr.replace(/%%M%%/g, calcage(secs,60,60));
-  DisplayStr = DisplayStr.replace(/%%S%%/g, calcage(secs,1,60));
-
-  document.getElementById("cntdwn").innerHTML = DisplayStr;
-  if (CountActive)
-    setTimeout("CountBack(" + (secs+CountStepper) + ")", SetTimeOutPeriod);
-}
-
-function putspan(backcolor, forecolor) {
- document.write("<span id='cntdwn' style='font-size:18px; background-color:" + backcolor + 
-                "; color:" + forecolor + "'></span>");
-}
-
-if (typeof(BackColor)=="undefined")
-  BackColor = "white";
-if (typeof(ForeColor)=="undefined")
-  ForeColor= "#black";
-if (typeof(TargetDate)=="undefined")
-  TargetDate = "02/15/2017 00:00 AM";
-if (typeof(DisplayFormat)=="undefined")
-  DisplayFormat = "<br>Time left: %%D%% Days, %%H%% Hours, %%M%% Minutes, %%S%% Seconds.";
-if (typeof(CountActive)=="undefined")
-  CountActive = true;
-if (typeof(FinishMessage)=="undefined")
-  FinishMessage = "";
-if (typeof(CountStepper)!="number")
-  CountStepper = -1;
-if (typeof(LeadingZero)=="undefined")
-  LeadingZero = true; 
-
-
-CountStepper = Math.ceil(CountStepper);
-if (CountStepper == 0)
-  CountActive = false;
-var SetTimeOutPeriod = (Math.abs(CountStepper)-1)*1000 + 990;
-putspan(BackColor, ForeColor);
-var dthen = new Date(TargetDate);
-var dnow = new Date();
-if(CountStepper>0)
-  ddiff = new Date(dnow-dthen);
-else
-  ddiff = new Date(dthen-dnow);
-gsecs = Math.floor(ddiff.valueOf()/1000);
-CountBack(gsecs);
-</script>
-          </p>
+               <b>Close date:</b> 
+                <ul>
+                  <li>02/15/2017 00:00 AM</li>
+                </ul> </p>
               <br><br>
               <p class="text-left">
                <b>Hints:<b/> 
