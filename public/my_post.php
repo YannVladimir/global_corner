ff91e463739069d93d62ad9e0a2c15a19ecd67dc
YannVladimir <bavlaya549@gmail.com>
2017-02-08 09:03:10 +0200

    AD

--- public/my_post.php
+++ public/my_post.php
@@ -19,32 +19,32 @@ $query = "SELECT * FROM items where post_id = '{$id}'";
 $res = mysqli_query($con,$query); 
 while($row = mysqli_fetch_assoc($res)) 
 {
-	if($row['photo1'] == '')
+	if($row['photo1'] == '' || $row['photo1'] == 'error')
 	{
 		require('post-one.php');
 		exit;
 	}
-	elseif ($row['photo2'] == '')
+	elseif ($row['photo2'] == '' || $row['photo2'] == 'error')
 	{
 		require('post-two.php');
 		exit;
 	}
-	elseif ($row['photo3'] == '')
+	elseif ($row['photo3'] == '' || $row['photo3'] == 'error')
 	{
 		require('post-three.php');
 		exit;
 	}
-	elseif ($row['photo4'] == '')
+	elseif ($row['photo4'] == '' || $row['photo4'] == 'error')
 	{
 		require('post-four.php');
 		exit;
 	}	
-	elseif ($row['photo5'] == '')
+	elseif ($row['photo5'] == '' || $row['photo5'] == 'error')
 	{
 		require('post-five.php');
 		exit;
 	}
-	elseif ($row['photo6'] == '')
+	elseif ($row['photo6'] == '' || $row['photo6'] == 'error')
 	{
 		require('post-six.php');
 		exit;
