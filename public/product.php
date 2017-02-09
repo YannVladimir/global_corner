d80409dd8bdd9ddae92aa645fa87f6a929cd55d2
YannVladimir <bavlaya549@gmail.com>
2017-02-09 15:54:43 +0200

    VS

--- public/product.php
+++ public/product.php
@@ -10,32 +10,32 @@ $query = "SELECT * FROM items where post_id = '{$id}' and is_accepted = 1";
 $res = mysqli_query($con,$query); 
 while($row = mysqli_fetch_assoc($res))
 {
-	if($row['photo1'] == '')
+	if($row['photo1'] == ''|| $row['photo1'] == 'error')
 	{
 		require('product_one.php');
 		exit;
 	}
-	elseif ($row['photo2'] == '')
+	elseif ($row['photo2'] == '' || $row['photo2'] == 'error')
 	{
 		require('product_two.php');
 		exit;
 	}
-	elseif ($row['photo3'] == '')
+	elseif ($row['photo3'] == '' || $row['photo3'] == 'error' )
 	{
 		require('product_three.php');
 		exit;
 	}
-	elseif ($row['photo4'] == '')
+	elseif ($row['photo4'] == '' || $row['photo4'] == 'error')
 	{
 		require('product_four.php');
 		exit;
 	}	
-	elseif ($row['photo5'] == '')
+	elseif ($row['photo5'] == '' || $row['photo5'] == 'error')
 	{
 		require('product_five.php');
 		exit;
 	}
-	elseif ($row['photo6'] == '')
+	elseif ($row['photo6'] == '' || $row['photo6'] == 'error')
 	{
 		require('product_six.php');
 		exit;
