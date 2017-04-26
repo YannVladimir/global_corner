6e29cfaa719d8115cec66b23bbe82e41124e5b0a
YannVladimir <bavlaya549@gmail.com>
2017-04-26 16:14:18 +0200

    Update search.php

--- public/search.php
+++ public/search.php
@@ -1,8 +1,10 @@
 <div class="col-sm-4">
     <form action='search_results.php' method='GET'>
-      
-      <input type="text" name='k'  required="required" class="col-sm-2 form-control" placeholder="Search"/>
-      <select class="col-sm-5 form-control" name="category"  style="border-left-style: solid; border-left-width:1px; border-left-color:#504c4c;">
+      <div class="col-sm-5">
+         <input type="text" name='k'  required="required" class="form-control" placeholder="Search"/>
+      </div>
+        <div class="col-sm-5">
+          <select class="form-control" name="category"  style="border-left-style: solid; border-left-width:1px; border-left-color:#504c4c;">
           <option value="0">in all categories</option>
           <?php
             $q = "SELECT * from categories";
@@ -13,6 +15,9 @@
             }
           ?>
         </select>
-        <button type="submit" class="btn btn-default col-sm-1 bton form-control"><i class="fa fa-search"></i></button>
+        </div>
+        <div class="col-sm-2">
+        <button type="submit" class="btn btn-default bton"><i class="fa fa-search"></i></button>
+        </div>
       </form>
 </div>
