5179dcd0a3b07cdd164856428acfbd39cc8aee6a
YannVladimir <bavlaya549@gmail.com>
2017-05-16 15:09:19 +0200

    removed the active things

--- public/links.php
+++ public/links.php
@@ -3,11 +3,7 @@
                     $res = mysqli_query($con,$query);
                     while($row = mysqli_fetch_assoc($res))
                     {
-                      if($active == $row['cat_id'])
-                      {
-                        echo "<li><a href='category.php?id={$row['cat_id']}&active={$row['cat_id']}' class='active'>{$row['cat_name']}</a>";
-                      }
-                      else
+              
                        echo"<li><a href='category.php?id={$row['cat_id']}&active={$row['cat_id']}' class=''>{$row['cat_name']}</a> ";
                       
                     }
