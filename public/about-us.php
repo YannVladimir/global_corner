4f3a0888c8a4f16212d84694977226a5e30d362f
YannVladimir <bavlaya549@gmail.com>
2017-05-16 08:04:42 +0200

    Update about-us.php

--- public/about-us.php
+++ public/about-us.php
@@ -89,7 +89,7 @@ require_once ('../includes/main_functions.php');
     <div class="header-bottom"><!--header-bottom-->
       <div class="container">
         <div class="row">
-          <div class="col-sm-12">
+          <div class="col-md-12">
             <div class="navbar-header">
               <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                 <span class="sr-only">Toggle navigation</span>
