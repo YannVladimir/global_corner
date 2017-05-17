efb53452c1ca29979ba2ec6bc9037f48eb545c67
YannVladimir <bavlaya549@gmail.com>
2017-05-17 19:25:22 +0200

    Update slider.php

--- public/slider.php
+++ public/slider.php
@@ -90,7 +90,7 @@
 					
 				</div>
 				<div class="col-sm-2">
-					<img src="assets/images/home/advertise.png">
+					<img src="assets/images/home/global.jpg">
 				</div>
 				<!--<div class="col-sm-3">
 				<h2 class="title text-center">Job vacancies</h2>
