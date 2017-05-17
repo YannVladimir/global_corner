77c34e57fd8763e16018b393e1b903fa4870160a
YannVladimir <bavlaya549@gmail.com>
2017-05-17 15:28:51 +0200

    Update job_vacancies.php

--- public/job_vacancies.php
+++ public/job_vacancies.php
@@ -63,7 +63,7 @@ checkToken();
     <div class="header-bottom"><!--header-bottom-->
       <div class="container">
         <div class="row">
-          <div class="col-sm-7">
+          <div class="col-sm-12">
             <div class="navbar-header">
               <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                 <span class="sr-only">Toggle navigation</span>
@@ -74,19 +74,23 @@ checkToken();
             </div>
             <div class="mainmenu pull-left">
               <ul class="nav navbar-nav collapse navbar-collapse">
-                 <li><a href="home.php" class="fon">Home</a></li>
-                <li><a href="upload.php" class="fon">Post your ad</a></li>
-                <li><a href="order.php" class="fon">Order now</a></li>
-                <li><a href="categories.php" class="fon">Products</a></li>
-                <li><a href="services.php" class="fon">Services</a></li>
-                <li><a href="job_vacancies.php" class="active fon">Job vacancies</a></li>
-                
+                 <li><a href="home.php">Home</a></li>
+                <?php
+                    $query = "SELECT * from categories where cat_id !=7";
+                    $res = mysqli_query($con,$query);
+                    while($row = mysqli_fetch_assoc($res))
+                    {
+              
+                       echo"<li><a href='category.php?id={$row['cat_id']}&active={$row['cat_id']}' class=''>{$row['cat_name']}</a></li> ";
+                      
+                    }
+                ?>
+<li><a href="job_vacancies.php" class="active">Job Vacancies</a></li> 
+
                             </ul>
                         </div>
                     </div>
-                    <?php
-                      include('search.php');
-                    ?>
+                    
                 </div>
             </div>
         </div><!--/header-bottom-->
